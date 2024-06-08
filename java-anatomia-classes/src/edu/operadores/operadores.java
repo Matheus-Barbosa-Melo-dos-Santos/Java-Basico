1079e9d6d7e2f9b23638e97d7f00e20537449018
Matheus Barbosa <132953818+Matheus-Barbosa-Melo-dos-Santos@users.noreply.github.com>
2024-06-07 22:11:55 -0300

    add package operadores

---
+++ java-anatomia-classes/src/edu/operadores/operadores.java
@@ -0,0 +1,16 @@
+package edu.operadores;
+
+public class operadores {
+    
+    public static void main(String[] args) {
+        double soma = 10.5 + 15.7;
+        int subtracão = 113-25;
+        int multiplicacao = 20*7; 
+        int divisao = 15 / 3 
+        int modulo = 18 % 3;
+        double resultado = (10 + 7) + (20/4);
+
+        System.out.println(resultado);
+    }
+    
+}
--- java-anatomia-classes/src/edu/variaveis/TiposVariaveis.java
+++ java-anatomia-classes/src/edu/variaveis/TiposVariaveis.java
@@ -1,6 +1,9 @@
 package edu.variaveis;
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
+        double salarioMinimo = 2500; 
+        int numero = 1;
+        short numeral = 1; 
+
     }
 }
