e24c303736f7e704988faa0ff063dc1dc9953444
Matheus Barbosa <132953818+Matheus-Barbosa-Melo-dos-Santos@users.noreply.github.com>
2024-06-07 22:47:35 -0300

    Save exercices

--- java-anatomia-classes/src/edu/operadores/operadores.java
+++ java-anatomia-classes/src/edu/operadores/operadores.java
@@ -3,14 +3,22 @@ package edu.operadores;
 public class operadores {
     
     public static void main(String[] args) {
-        double soma = 10.5 + 15.7;
-        int subtracão = 113-25;
-        int multiplicacao = 20*7; 
-        int divisao = 15 / 3 
-        int modulo = 18 % 3;
-        double resultado = (10 + 7) + (20/4);
+        String nomeUm = "Matheus";
+        String nomeDois = new String ("Matheus");
 
-        System.out.println(resultado);
+        System.out.println(nomeUm == nomeDois);
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+        boolean simNao = numero1 != numero2;
+
+        if (numero1 < numero2) {
+            System.out.println("Nossa condição é verdadeira");
+        }
+        
+        System.out.println(simNao);
     }
     
 }
--- java-anatomia-classes/src/edu/variaveis/TiposVariaveis.java
+++ java-anatomia-classes/src/edu/variaveis/TiposVariaveis.java
@@ -2,8 +2,10 @@ package edu.variaveis;
 public class TiposVariaveis {
     public static void main(String[] args) {
         double salarioMinimo = 2500; 
-        int numero = 1;
-        short numeral = 1; 
+       /* int numero = 1;
+        short numeral = 1; */
+
+        System.out.println(salarioMinimo);
 
     }
 }
